7d4ede9c481978cb6e7f76729984576eae046dc0
Kuber Gupta <kuber8821@gmail.com>
2024-04-21 00:09:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
